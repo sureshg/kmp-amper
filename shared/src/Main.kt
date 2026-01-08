9a1ceb4f11a05a74eba36fc7cd8c2b6a88b93f0e
Suresh G <sureshg.g@gmail.com>
2026-01-08 10:48:13 -0800

    chore: kotlin 2.3.20-Beta1 changes

--- shared/src/Main.kt
+++ shared/src/Main.kt
@@ -1,9 +1,11 @@
 import dev.suresh.*
+import io.github.oshai.kotlinlogging.*
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking { mediaClient() }
 
 suspend fun mediaClient() {
+  KotlinLoggingConfiguration.logStartupMessage = false
   println("Kotlin ${KotlinVersion.CURRENT} - ${Platform().name()}")
   val client = MediaApiClient()
   println(client)
