3e7a1e74abdca39595e1805c8ab3aa116c3b1415
Suresh G <sureshg.g@gmail.com>
2026-02-11 14:59:58 -0800

    chore: add lumis4j syntax highlighting and dep updates

--- jvm/app/src/App.kt
+++ jvm/app/src/App.kt
@@ -1,10 +1,35 @@
 import ffm.*
 import genai.voices
+import io.roastedroot.lumis4j.core.*
 
 suspend fun main() {
   System.setProperty("slf4j.internal.verbosity", "WARN")
   println(PosixUser)
   mediaClient()
   vectorApi()
+  syntaxHighlight()
   voices()
+}
+
+fun syntaxHighlight() {
+  Lumis.builder().build().use {
+    val h = it.highlighter().withLang(Lang.KOTLIN).withTheme(Theme.GITHUB_DARK).build()
+    println(
+        h.highlight(
+                $$"""
+                fun main(args: Array<String>) =
+                    try {
+                      initProps()
+                      EngineMain.main(args)
+                    } catch (e: Throwable) {
+                      val log = KtorSimpleLogger("main")
+                      log.error("Failed to start the app: ${e.message}", e)
+                    }
+
+                """
+                    .trimIndent()
+            )
+            .string()
+    )
+  }
 }
