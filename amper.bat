b63428460af0bfddcfd42696c8c6c80b910d6117
Suresh G <sureshg.g@gmail.com>
2026-03-09 14:52:41 -0700

    Update Amper to 0.10.0-dev-3712 and dependency updates

--- amper.bat
+++ amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.10.0-dev-3708
+set amper_version=0.10.0-dev-3712
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=a2a67a97e01dcd6856e0a75c6697f2082e62fe6f235f09656498f767d8ddf981
+set amper_sha256=922143fe355b6222108ac627b000354aa5ab293af4e58de37714c984b393476b
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
