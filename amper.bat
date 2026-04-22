fc16db5a17b84d6d9650e577e90b7100c25775bf
Suresh G <sureshg.g@gmail.com>
2026-04-21 17:14:11 -0700

    chore: Update Amper, Compose, OpenTelemetry, GraalVM, and dependencies

--- amper.bat
+++ amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.11.0-dev-3856
+set amper_version=0.11.0-dev-3863
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=2ac2139731ce029f7968de8fbafeb53f7ba53781cdabe8f1755ab25d79e896d8
+set amper_sha256=04b59d8fa6fa44a8833746ea0fbef705260137fa78bc108ebeea7211378aa0d4
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
