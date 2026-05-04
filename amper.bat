a7bd1ed841f027d128caca1f8ad23a4d11cdaee5
Suresh G <sureshg.g@gmail.com>
2026-05-04 08:13:20 -0700

    chore: Update Amper, enable powerAssert, and update dependencies

--- amper.bat
+++ amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.11.0-dev-3890
+set amper_version=0.11.0-dev-3907
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=5f3a8a8905af1dc83100fb04175fc18f0057ebb67a4624c5fa05b6c075a210d1
+set amper_sha256=6f2357ff1d34ba4c185fa61e04feebf397dd2a025940777ad97776ea8821c1ff
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
