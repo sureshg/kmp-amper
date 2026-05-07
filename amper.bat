50b8734ca6c4de6c5c167a1c96b2c55ab15fbb2a
Suresh G <sureshg.g@gmail.com>
2026-05-07 09:00:23 -0700

    chore: Update Amper and dependencies

--- amper.bat
+++ amper.bat
@@ -17,9 +17,9 @@
 setlocal
 
 @rem The version of the Amper distribution to provision and use
-set amper_version=0.11.0-dev-3915
+set amper_version=0.11.0-dev-3916
 @rem Establish chain of trust from here by specifying exact checksum of Amper distribution to be run
-set amper_sha256=c811b26dd4c22ca2f6059f67e5c051e8f6f3b9c5053ef0450da5a3d7f1fbf18b
+set amper_sha256=f4e08de542fd1e6bb2dea31372b71eda128659a201fcf4dac5508812fa25d7e1
 
 if not defined AMPER_DOWNLOAD_ROOT set AMPER_DOWNLOAD_ROOT=https://packages.jetbrains.team/maven/p/amper/amper
 if not defined AMPER_JRE_DOWNLOAD_ROOT set AMPER_JRE_DOWNLOAD_ROOT=https:/
